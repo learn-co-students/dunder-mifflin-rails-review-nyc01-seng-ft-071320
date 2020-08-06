16c7afab417d726e230a0aa3d332b8424414cb4b
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-06 14:52:31 -0400

    migrate, add dogs view

--- db/migrate/20180411182746_create_employees.rb
+++ db/migrate/20180411182746_create_employees.rb
@@ -5,6 +5,7 @@ class CreateEmployees < ActiveRecord::Migration[5.1]
       t.string :last_name
       t.string :alias
       t.string :title
+      t.integer :dog_id
       t.string :office
       t.string :img_url
 
