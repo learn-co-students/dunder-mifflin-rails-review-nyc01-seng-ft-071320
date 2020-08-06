b6a698e8820efcbca1c6d0cf79ffd4e97afed452
victorscholz <vgscholz@gmail.com>
2020-08-06 18:09:09 -0400

    finished, still need to complete bonus dog sort

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, uniqueness: { 
+        message: "Please use a unique title"
+    }
+    validates :alias, uniqueness: { 
+        message: "Please use a unique alias"
+    }
+
 end
