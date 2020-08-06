2079f811f2ae3efae1162c50235c7fc33eedcbd5
David-Kirsch <davidjkirsch@gmail.com>
2020-08-06 16:17:44 -0400

    CRUD ability to employee_controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,6 +5,35 @@ class EmployeesController < ApplicationController
     end
 
     def show
+        @employee = find_employee
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(param_settings)
+        redirect_to employee_path(@employee)
+    end
+
+    def edit
+        @employee = find_employee
+    end
+
+    def update
+        @employee = find_employee
+        @employee.update(param_settings)
+        redirect_to employee_path(@employee)
+    end
+
+    private
+
+    def find_employee
         @employee = Employee.find(params[:id])
+    end
+
+    def param_settings
+        params.require(:employee).permit(:title, :first_name, :last_name, :alias, :office, :img_url, :dog_id)
     end
 end
--- db/migrate/20200806195530_change_dog_to_dog_id.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDogToDogId < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :employees, :dog, :dog_id
-  end
-end
