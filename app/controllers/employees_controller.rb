bbaf2a1c942aad97b2239db8b8f3e30f5df7dc7b
junko911 <junkotahara911@gmail.com>
2020-08-06 15:58:20 -0400

    Did everything besides bonus

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,4 +7,24 @@ class EmployeesController < ApplicationController
     def show
         @employee = Employee.find(params[:id])
     end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        if @employee.valid?
+            redirect_to employee_path(@employee)
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
