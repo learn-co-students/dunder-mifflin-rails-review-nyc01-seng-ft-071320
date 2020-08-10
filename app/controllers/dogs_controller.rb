51ac453e4dcf8ca8c740d653a65cb2247b6a012f
Ryan Flynn <ryanflynn44@gmail.com>
2020-08-09 23:33:04 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,24 @@
 class DogsController < ApplicationController
+    
+    before_action :find_dog, only: [:show]
+
+    def index
+        @dogs = Dog.all.sort_by {|dog| -dog.employees.count}
+    end
+
+    def show
+        
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end
+
+
 end
